524138e9906af98e2d4248c79bea9c5d15fa7f73
TriHalder2 <73516625+TriHalder2@users.noreply.github.com>
2020-10-28 17:26:27 +0530

    Add files via upload

---
+++ JavaTraining/Activity1_3.java
@@ -0,0 +1,31 @@
+
+public class Activity1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double AgeSeconds = 1000000000;
+
+		double Earth = 31.69;
+		double Mercury = 0.2408467;
+		double Venus = 0.61519726;
+		double Mars = 1.8808158;
+		double Jupiter = 11.862615;
+		double Saturn = 29.447498;
+		double Uranus = 84.016846;
+		double Neptune = 164.79132;
+		
+				        
+		        System.out.println("Age on Mercury: " + Earth / Mercury);
+		        System.out.println("Age on Venus: " + Earth / Venus);
+		        System.out.println("Age on Earth: " +  Earth);
+		        System.out.println("Age on Mars: " + Earth/ Mars);
+		        System.out.println("Age on Jupiter: " + Earth/ Jupiter);
+		        System.out.println("Age on Saturn: " + Earth/ Saturn);
+		        System.out.println("Age on Uranus: " + Earth / Uranus);
+		        System.out.println("Age on Neptune: " +Earth / Neptune);
+		    }
+		
+
+	}
+
+
