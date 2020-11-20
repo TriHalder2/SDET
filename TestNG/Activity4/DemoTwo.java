8fe328ab62f97890c484218097d219557c12dbb3
TriHalder2 <73516625+TriHalder2@users.noreply.github.com>
2020-11-20 19:23:58 +0530

    Add files via upload

---
+++ TestNG/Activity4/DemoTwo.java
@@ -0,0 +1,12 @@
+package TestNGTraining;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+	   @Test
+	    public void TestCase() {
+	        System.out.println("I'm in the test case from DemoTwo Class");
+	    }
+	}
+
+
